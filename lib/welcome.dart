d58ecb5957fb3809d0078b3f7a27e4e717cb25ed
Ezra Bathini <ezra.bathini@vodafone.com>
2018-06-25 00:42:56 +1200

    updates welcome screen

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,88 +1,47 @@
 import 'package:flutter/material.dart';
 
-import 'service/dbt.dart';
-import 'model/book.dart';
-import 'model/verse.dart';
-import 'fixtures/bookdata.dart';
 import 'colors.dart';
 
 class WelcomePage extends StatelessWidget {
-  Widget _buildWelcomeVerse(BuildContext context, Verse welcomeVerse) {
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      padding: EdgeInsets.all(10.0),
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
+  Widget _startButton(BuildContext context) {
+    return new Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RaisedButton(
+        padding: EdgeInsets.all(10.0),
+        elevation: 8.0,
+        color: kPrimaryColor,
+        textColor: kSurfaceDark,
+        child: Text('START READING'),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(3.0)),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Text(
-                  welcomeVerse.verseText,
-                  style: TextStyle(color: kPrimaryColor, fontSize: 20.0),
-                  textAlign: TextAlign.end,
-                ),
-                RichText(
-                  textAlign: TextAlign.end,
-                  text: TextSpan(children: [
-                    TextSpan(
-                      text: welcomeVerse.bookName.toString(),
-                      style: TextStyle(
-                          color: kPrimaryColor,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    TextSpan(
-                      text: ' ',
-                      style: TextStyle(color: kPrimaryColor, fontSize: 20.0),
-                    ),
-                    TextSpan(
-                      text:
-                          '${welcomeVerse.chapterId.toString()}:${welcomeVerse.verseId.toString()}',
-                      style: TextStyle(color: kPrimaryColor, fontSize: 20.0),
-                    ),
-                  ]),
-                ),
-              ],
-            ),
-          ),
           Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
-              new Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  padding: EdgeInsets.all(8.0),
-                  elevation: 0.0,
-                  color: kPrimaryColor,
-                  textColor: kSurfaceWhite,
-                  child: Text(
-                      'Read more from ${welcomeVerse.bookName} Chapter ${welcomeVerse.chapterId}'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
+              Text(
+                'The Bible',
+                style: TextStyle(
+                    color: kPrimaryColor,
+                    fontSize: 50.0,
+                    fontFamily: 'Cinzel',
+                    fontWeight: FontWeight.w300),
+                textAlign: TextAlign.end,
               ),
-              new Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  padding: EdgeInsets.all(8.0),
-                  elevation: 8.0,
-                  color: kSurfaceWhite,
-                  textColor: kPrimaryColor,
-                  child: Text('START'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(3.0)),
-                  ),
-                ),
-              ),
+              _startButton(context)
             ],
           ),
         ],
@@ -90,26 +49,11 @@ class WelcomePage extends StatelessWidget {
     );
   }
 
-  // builder: _buildWelcomeVerse(context, welcomeVerse),
-//
   @override
   Widget build(BuildContext context) {
-    Book welcomeBook = getBookForWelcome();
-
     return new Scaffold(
-      body: new Center(
-        child: FutureBuilder(
-          future: fetchVerseFromChapter(welcomeBook.bookId, welcomeBook.damId,
-              welcomeBook.getRandomChapter().toString()),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return _buildWelcomeVerse(context, snapshot.data);
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            return new CircularProgressIndicator();
-          },
-        ),
+      body: Center(
+        child: _buildTitle(context),
       ),
     );
   }

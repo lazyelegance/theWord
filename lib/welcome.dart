1046c33b8cfd39e65bef127532e0db25bffd64fa
Ezra Bathini <ezrabathini@gmail.com>
2018-07-08 09:36:06 +1200

    updates welcome page styling

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -6,45 +6,66 @@ class WelcomePage extends StatelessWidget {
   Widget _startButton(BuildContext context) {
     return new Padding(
       padding: const EdgeInsets.all(8.0),
-      child: RaisedButton(
+      child: FlatButton(
         padding: EdgeInsets.all(10.0),
-        elevation: 8.0,
-        color: kPrimaryColor,
-        textColor: kSurfaceDark,
+        color: kSurfaceWhite,
+        textColor: Colors.black,
         child: Text('START READING'),
         onPressed: () {
           Navigator.pop(context);
         },
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(3.0)),
+          borderRadius: BorderRadius.all(Radius.circular(1.0)),
         ),
       ),
     );
   }
 
-  Widget _buildTitle(BuildContext context) {
+  Widget _buildContent(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(10.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Text(
-                'The Bible',
-                style: TextStyle(
-                    color: kPrimaryColor,
-                    fontSize: 50.0,
-                    fontFamily: 'Cinzel',
-                    fontWeight: FontWeight.w300),
-                textAlign: TextAlign.end,
-              ),
-              _startButton(context)
-            ],
+            children: <Widget>[_buildTitle(), _startButton(context)],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      margin: EdgeInsets.only(right: 10.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black45,
+          width: 2.0,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(1.0)),
+      ),
+      child: SizedBox(
+        height: 180.0,
+        width: 180.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              'The Bible',
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 45.0,
+                  fontFamily: 'Rubik',
+                  fontWeight: FontWeight.w100),
+              textAlign: TextAlign.end,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -53,7 +74,7 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Center(
-        child: _buildTitle(context),
+        child: _buildContent(context),
       ),
     );
   }

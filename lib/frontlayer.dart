342a3b16079e035497b589777ccf06a16c567222
ezra bathini <ezrabathini@gmail.com>
2019-06-22 20:11:46 +1200

    updates styling

--- lib/frontlayer.dart
+++ lib/frontlayer.dart
@@ -15,42 +15,77 @@ class FrontLayer extends StatelessWidget {
     return Material(
       elevation: 16.0,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0)),
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(23.0)),
       ),
-      child: Scaffold(
-        body: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            GestureDetector(
-              behavior: HitTestBehavior.opaque,
-              onTap: onTap,
-              child: Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: onTap,
+            child: Container(
+              decoration: BoxDecoration(
                 color: kSurfaceWhite,
-                width: 48.0,
-                alignment: AlignmentDirectional.centerStart,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(23.0)),
               ),
+              width: 40.0,
+              alignment: AlignmentDirectional.centerStart,
             ),
-            Expanded(
-              child: child,
-            ),
-          ],
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: kSurfaceWhite,
-          child: Container(
-            height: 10.0,
-            color: kSurfaceWhite,
           ),
-          hasNotch: true,
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.keyboard_arrow_right),
-          onPressed: onPressNext,
-          backgroundColor: kSurfaceWhite,
-          foregroundColor: kPrimaryColor,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+          Expanded(
+            child: child,
+          ),
+        ],
       ),
     );
   }
 }
+
+// class FrontLayer extends StatelessWidget {
+//   const FrontLayer({Key key, this.child, this.onTap, this.onPressNext})
+//       : super(key: key);
+
+//   final Widget child;
+//   final VoidCallback onTap;
+//   final VoidCallback onPressNext;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Material(
+//       elevation: 16.0,
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0)),
+//       ),
+//       child: Column(
+//         children: <Widget>[
+//           GestureDetector(
+//             behavior: HitTestBehavior.opaque,
+//             onTap: onTap,
+//             child: Container(
+//               color: kSurfaceWhite,
+//               height: 48.0,
+//               alignment: AlignmentDirectional.centerStart,
+//             ),
+//           ),
+//           Expanded(
+//             child: child,
+//           ),
+//         ],
+//         // bottomNavigationBar: BottomAppBar(
+//         //   color: kSurfaceWhite,
+//         //   child: Container(
+//         //     height: 10.0,
+//         //     color: kSurfaceWhite,
+//         //   ),
+//         // ),
+//         // floatingActionButton: FloatingActionButton(
+//         //   child: Icon(Icons.keyboard_arrow_right),
+//         //   onPressed: onPressNext,
+//         //   backgroundColor: kSurfaceWhite,
+//         //   foregroundColor: kPrimaryColor,
+//         // ),
+//         // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+//       ),
+//     );
+//   }
+// }
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-
-import 'colors.dart';
+import 'package:the_word/colors.dart';
 
 class WelcomePage extends StatelessWidget {
   Widget _startButton(BuildContext context) {
     return new Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: FlatButton(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+      child: RaisedButton(
         padding: EdgeInsets.all(10.0),
-        color: kSurfaceWhite,
-        textColor: Colors.black,
-        child: Text('START READING'),
+        child: Text(
+          'START READING',
+          style: TextStyle(color: Colors.white),
+        ),
+        color: kPrimaryColor,
         onPressed: () {
           Navigator.pop(context);
         },
@@ -23,7 +24,7 @@ class WelcomePage extends StatelessWidget {
 
   Widget _buildContent(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(10.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
@@ -40,32 +41,21 @@ class WelcomePage extends StatelessWidget {
   Widget _buildTitle() {
     return Container(
       padding: EdgeInsets.all(10.0),
-      margin: EdgeInsets.only(right: 10.0),
       decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black45,
-          width: 2.0,
-        ),
+        // border: Border.all(
+        //   color: Colors.black45,
+        //   width: 2.0,
+        // ),
         borderRadius: BorderRadius.all(Radius.circular(1.0)),
       ),
-      child: SizedBox(
-        height: 180.0,
-        width: 180.0,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            Text(
-              'The Bible',
-              style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 45.0,
-                  fontFamily: 'Rubik',
-                  fontWeight: FontWeight.w100),
-              textAlign: TextAlign.end,
-            ),
-          ],
-        ),
+      child: Text(
+        'The Bible',
+        style: TextStyle(
+            color: kPrimaryColor,
+            fontSize: 45.0,
+            fontFamily: 'Rubik',
+            fontWeight: FontWeight.w100),
+        textAlign: TextAlign.end,
       ),
     );
   }
